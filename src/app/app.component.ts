5018f699d5a597a6ccf087cf3cd2dae9e169d999
Martine Alliet <allietmartine@gmail.com>
2023-07-20 09:21:55 +0100

    commit thursday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ch13-ex500';
+  selectedAnimal = 'cat';
+  animals = ['cat', 'dog', 'zebra', 'giraffe'];
+
+  onAnimalClicked(event: Event) {
+    const clickedAnimal = (event.target as HTMLElement).innerHTML.trim();
+    this.selectedAnimal = clickedAnimal;
+  }
+
+  getAnimalStyle(animal:any) {
+    const isSelected = (animal === this.selectedAnimal);
+    return {
+      'padding': '10px',
+      'margin': '10px',
+      'color': isSelected ? '#ffffff' : '#000000',
+      'background-color': isSelected? '#ff0000':'#ffffff',
+    }
+  }
 }
